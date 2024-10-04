09dd2d3fce57a33a8bcbb7470e23a8efb0cf09f1
kumarkaji <153839042+kumarkaji@users.noreply.github.com>
2024-10-04 12:14:31 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region                   = "eu-west-1"
   shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
