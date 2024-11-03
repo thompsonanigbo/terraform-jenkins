6ee9d8845f83ec3397ff1f0eac1ece2172a3915f
thompsonanigbo <103883140+thompsonanigbo@users.noreply.github.com>
2024-11-03 17:39:54 +0000

    Update remote_backend_s3.tf

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-jenkins-remote-state-bucket-123456"
+    bucket = "thompsonanigbo-jenkins-remote-state-bucket-12345"
     key    = "devops-project-1/jenkins/terraform.tfstate"
-    region = "eu-west-1"
+    region = "eu-west-2"
   }
 }
